bd173352560144e8079fb9f842867bf23e28a949
Kyle Weingart <kyle.weingart@gmail.com>
2018-11-09 16:52:30 -0500

    added code to consume kml file with labels

--- Add_KML/kml-layer.js
+++ Add_KML/kml-layer.js
@@ -8,13 +8,33 @@ var raster = new ol.layer.Tile({
   })
 });
 
+var vector = new ol.layer.Vector({
+  source: new ol.source.Vector({
+    url: 'http://127.0.0.1:8082/AL682014_34_earliest_reasonable_toa_34.kml',
+    format: new ol.format.KML(),
+  })
+});
+
+// var vector = new ol.layer.Vector({
+//   source: new ol.source.Vector({
+//     url: 'https://openlayers.org/en/v3.20.1/examples/data/kml/2012-02-10.kml',
+//     format: new ol.format.KML()
+//   })
+// });
 // create map
 var map = new ol.Map({
-  layers: [raster],
+  layers: [raster, vector],
   target: document.getElementById('map'),
   view: new ol.View({
     center: ol.proj.transform([-97.6114, 38.8403], 'EPSG:4326', 'EPSG:3857'),
     zoom: 5
   })
 });
+
+var feature = map.forEachFeatureAtPixel(pixel, function(feature) {
+  console.log(feature);
+  return feature;
+});
+
+
 
