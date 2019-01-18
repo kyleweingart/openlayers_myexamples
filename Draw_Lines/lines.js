194a2276a858028d0a362133abcc7b1e2c4eb2bd
Kyle Weingart <kyle.weingart@gmail.com>
2019-01-18 11:44:19 -0500

    added line

--- Draw_Lines/lines.js
+++ Draw_Lines/lines.js
@@ -2,11 +2,25 @@ var raster = new ol.layer.Tile({
     source: new ol.source.OSM()
 })
 
+var linestring_feature = new ol.Feature({
+    geometry: new ol.geom.LineString(
+        [[0, -20000000], [0, 20000000]]
+        ),
+    title: 'Prime Meridian'
+});
+
+var vector = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        features: [linestring_feature]
+    })
+})
+
 var map = new ol.Map({
-    layers: [raster],
+    layers: [raster, vector],
     target: document.getElementById('map'),
     view: new ol.View({
         center: ol.proj.transform([-87.5, 31], 'EPSG:4326', 'EPSG:3857'),
+        center: [0,0],
         zoom:1
     }),
 });
