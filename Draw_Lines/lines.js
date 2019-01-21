c79c97d3047962db1cca5d5db4418ccb0273c083
Kyle Weingart <kyle.weingart@gmail.com>
2019-01-21 12:28:05 -0500

    added coords as object

--- Draw_Lines/lines.js
+++ Draw_Lines/lines.js
@@ -2,26 +2,32 @@ var raster = new ol.layer.Tile({
     source: new ol.source.OSM()
 })
 
-var coords = [[0, 0], [0, 85]];
-var coordsPrj = new ol.geom.LineString(coords);
-coordsPrj.transform('EPSG:4326', 'EPSG:3857');
-
-var linestring_feature = new ol.Feature({
-    geometry: coordsPrj,
-    title: 'Prime Meridian'
-});
-
 var vector = new ol.layer.Vector({
     source: new ol.source.Vector({
-        features: [linestring_feature]
+        features: []
     }),
     style: new ol.style.Style({
         stroke: new ol.style.Stroke({
             color: 'black',
-            width: 5
+            width: 3
         })
     })
 })
+
+var coords = [{coords:[[0, 0], [0, 85]], title: 'prime meredian'}, {coords:[[30, 0], [30, 85]], title: 'meridian'}, {coords:[[180, 0], [-180, 0]], title: 'equator'}];
+console.log(coords.length);
+
+for (var i=0; i <= 2; i++) {
+console.log(coords[i]);
+var coordsPrj = new ol.geom.LineString(coords[i].coords);
+coordsPrj.transform('EPSG:4326', 'EPSG:3857');
+console.log(coordsPrj);
+var linestring_feature = new ol.Feature({
+    geometry: coordsPrj,
+    title: coords[i].title
+})
+vector.getSource().addFeature(linestring_feature);
+};
 
 var map = new ol.Map({
     layers: [raster, vector],
