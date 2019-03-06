c9cd80f722f8ad70f9be75a52585716dba2ca518
Kyle Weingart <kyle.weingart@gmail.com>
2019-03-06 15:55:41 -0500

    working storm toggle

--- Grib2/grib-layers.js
+++ Grib2/grib-layers.js
@@ -18,42 +18,51 @@ var map = new ol.Map({
     ]
 });
 
-map.addLayer(new ol.layer.Tile({
-    source: new ol.source.TileWMS({
-        url: 'https://gis.srh.noaa.gov/arcgis/services/NDFDTemps/MapServer/WMSServer',
-        params: {
-            LAYERS: 16,
-            FORMAT: 'image/png',
-            TRANSPARENT: true
-        },
-        attributions: [
-            new ol.Attribution({
-                html: 'Data provided by the <a href="http://noaa.gov">NOAA</a>.'
-            })
-        ]
-    }),
-    opacity: 0.50
-}));
+// map.addLayer(new ol.layer.Tile({
+//     source: new ol.source.TileWMS({
+//         url: 'https://gis.srh.noaa.gov/arcgis/services/NDFDTemps/MapServer/WMSServer',
+//         params: {
+//             LAYERS: 16,
+//             FORMAT: 'image/png',
+//             TRANSPARENT: true
+//         },
+//         attributions: [
+//             new ol.Attribution({
+//                 html: 'Data provided by the <a href="http://noaa.gov">NOAA</a>.'
+//             })
+//         ]
+//     }),
+//     opacity: 0.50
+// }));
 
-map.addLayer(new ol.layer.Tile({
-    source: new ol.source.TileWMS({
-        url: 'http://ogc.bgs.ac.uk/cgi-bin/BGS_Bedrock_and_Superficial_Geology/wms',
-        params: {
-            LAYERS: 'BGS_EN_Bedrock_and_Superficial_Geology'
-        },
-        attributions: [
-            new ol.Attribution({
-                html: 'Contains <a href="http://bgs.ac.uk/">British Geological Survey</a> ' +
-                      'materials &copy; NERC 2015'
-            })
-        ]
-    }),
-    opacity: 0.85
-}));
+// map.addLayer(new ol.layer.Tile({
+//     source: new ol.source.TileWMS({
+//         url: 'http://ogc.bgs.ac.uk/cgi-bin/BGS_Bedrock_and_Superficial_Geology/wms',
+//         params: {
+//             LAYERS: 'BGS_EN_Bedrock_and_Superficial_Geology'
+//         },
+//         attributions: [
+//             new ol.Attribution({
+//                 html: 'Contains <a href="http://bgs.ac.uk/">British Geological Survey</a> ' +
+//                       'materials &copy; NERC 2015'
+//             })
+//         ]
+//     }),
+//     opacity: 0.85
+// }));
 
-document.getElementById('js-layers').addEventListener('change', function() {
-    var values = this.value.split(',');
-    var view = map.getView();
-    view.setCenter([parseFloat(values[0]), parseFloat(values[1])]);
-    view.setZoom(values[2]);
-});
+// document.getElementById('js-layers').addEventListener('change', function() {
+//     var value = this.value;
+//     console.log(value);
+   
+// });
+
+var radios = document.forms["windprobsForm"].elements["windprobs"];
+for(var i = 0; i < radios.length; i++) {
+    radios[i].onclick = function() {
+        console.log(this.value);
+    }
+}
+// document.getElementsByClassName('.layer').addEventListener('change', function() {
+//     console.log(this.value);
+// });
