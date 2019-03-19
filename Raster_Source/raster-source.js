aea112a553b236855c75f8a063e8af02fb83d156
Kyle Weingart <kyle.weingart@gmail.com>
2019-03-19 15:55:25 -0400

    working ol3 raster source

--- Raster_Source/raster-source.js
+++ Raster_Source/raster-source.js
@@ -1,33 +1,24 @@
-var basemap = new ol.layer.Tile({
-  source: new ol.source.OSM()
-})
-
-
 
 var codes = [];
 
 codes.push('w320i1');
-//codes.push('nnw320i1');
-//codes.push('wnw320i1');
+codes.push('nnw320i1');
+codes.push('wnw320i1');
 codes.push('e320i1');
-//codes.push('ene320i1');
-//codes.push('nne320i1');
+codes.push('ene320i1');
+codes.push('nne320i1');
 var surgeLayers = [];
 console.log(surgeLayers);
 
 for (var i = 0; i < codes.length; i++) {
-  var imgLayer = new ol.layer.Image({
-    extent: [-13884991, 2870341, -7455066, 6338219],
-    source: new ol.source.ImageWMS({
+  var imgSource = new ol.source.ImageWMS({
       url: 'http://localhost:8080/geoserver/wms',
       crossOrigin: 'anonymous',
       params: {'LAYERS': 'test:sf1', 'DIM_MAX_COLS': codes[i]},
       ratio: 1,
       serverType: 'geoserver'
-    }),
-  name: 'layer' + codes[i]
   });
-  surgeLayers.push(imgLayer);
+  surgeLayers.push(imgSource);
 }
 
 function getMaxPixel(pixel) {
@@ -45,9 +36,8 @@ function getMaxPixel(pixel) {
   return max;
 }
 
-console.log(surgeLayers);
 var surgeRaster = new ol.source.Raster({
-  sources: [surgeLayers],
+  sources: surgeLayers,
   /**
    * Run calculations on pixel data.
    * @param {Array} pixels List of pixels (one per source).
@@ -55,11 +45,8 @@ var surgeRaster = new ol.source.Raster({
    * @return {Array} The output pixel.
    */
   operation: function(pixels, data) {
-    console.log('here');
-    console.log(pixels);
     var value = getMaxPixel(pixels);
-    var pixel = value
-    
+    pixel = value;
     return pixel;
   },
   lib: {
@@ -69,18 +56,22 @@ var surgeRaster = new ol.source.Raster({
 
 
 
-surgeRaster.on('beforeoperations', function(event) {
-  console.log('before');
-});
+// surgeRaster.on('beforeoperations', function(event) {
+//   console.log('before');
+// });
 
 var map = new ol.Map({
-  layers: [basemap],
-  // layers: [basemap, new ol.layer.Image({
-  //   source: surgeRaster
-  // })],
-  target: document.getElementById('map'),
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    }),
+    new ol.layer.Image({
+      source: surgeRaster
+    })
+  ],
+  target: 'map',
   view: new ol.View({
-    center: ol.proj.transform([-97.6114, 38.8403], 'EPSG:4326', 'EPSG:3857'),
-    zoom: 5,
+    center: ol.proj.fromLonLat([-80.6114, 26.8403]),
+    zoom: 8
   }),
 });
