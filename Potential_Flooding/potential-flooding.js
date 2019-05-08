c521aca8e81ee91b22cd1d6d7ddfcbc07827c4d3
Kyle Weingart <kyle.weingart@gmail.com>
2019-05-08 15:05:55 -0400

    added new tiff source/layer

--- Potential_Flooding/potential-flooding.js
+++ Potential_Flooding/potential-flooding.js
@@ -4,10 +4,16 @@
       url: 'http://localhost:8080/geoserver/wms',
       params: {
         'LAYERS': 'nhp:GORDON_2018_adv08_e10_ResultRaster',
-        'VERSION:': '1.1.1',
-        'FORMAT': 'image/gif'
       },
-      projection: 'EPSG:3857',
+      ratio: 1,
+      serverType: 'geoserver'
+  });
+
+  var img2Source = new ol.source.TileWMS({
+      url: 'http://localhost:8080/geoserver/wms',
+      params: {
+        'LAYERS': 'nhp:HERMINE_2016_adv21_e10_ResultRaster',
+      },
       ratio: 1,
       serverType: 'geoserver'
   });
@@ -20,6 +26,9 @@ var map = new ol.Map({
     new ol.layer.Tile({
       source: imgSource
     }),
+    new ol.layer.Tile({
+      source: img2Source
+    })
     // new ol.layer.Image({
     //   source: testSource
     // })
@@ -30,12 +39,3 @@ var map = new ol.Map({
     zoom:5
   }),
 });
-
-// source: new ol.source.ImageWMS({
-//   url: 'http://localhost:8080/geoserver/nhp/wms',
-//   params: {
-//       'LAYERS': 'nhp:geotiff_data',
-//       'VERSION': '1.1.1',
-//       'FORMAT': 'image/gif'
-//   },
-//   projection: 'EPSG:3857',
