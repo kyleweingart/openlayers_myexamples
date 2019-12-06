d1495d6e8fee42697e8e7fb3f3a6f49bf40babb8
Kyle Weingart <kyle.weingart@gmail.com>
2019-12-06 17:30:01 -0500

    added some logic for QC

--- Grib2-WPS-TOAPointReport/grib-layers.js
+++ Grib2-WPS-TOAPointReport/grib-layers.js
@@ -2,15 +2,18 @@
 var map = new ol.Map({
   view: new ol.View({
     zoom: 4,
-    center: [-10527519, 3160212]
+    center: [-10527519, 3160212],
+    // proj: 'EPSG:4326'
   }),
   target: 'js-map',
   layers: [
     new ol.layer.Tile({
       source: new ol.source.OSM()
-    })
+    }), 
   ]
 });
+
+
 
 
 var createGrib2Layer = (strength) => {
@@ -28,22 +31,27 @@ var createGrib2Layer = (strength) => {
     var layers = 'VAR0-2-229_FROM_7-10--1_height_above_ground_120_Hour_Accumulation_probability_between_10p0_and_3'
   } else if (strength === 'LRTOD') {
     var layers = 'VAR0-2-230_FROM_7-10--1_height_above_ground_120_Hour_Accumulation_probability_between_10p0_and_3'
+  } else if (strength === 'test') {
+    var layers = 'cite:test.25'
   }
 
   var layer = new ol.layer.Tile({
     source: new ol.source.TileWMS({
       url: url,
       params: {
-        'LAYERS': 'ncdc:' + layers,
+        // 'LAYERS': 'ncdc:' + layers,
+        'LAYERS': layers,
         'TILED': true,
         'VERSION': '1.1.1',
         'FORMAT': 'image/png8',
+        // 'projection': 'EPSG:4326'
         // 'TIME': '2018-10-10T00:00:00Z'
       }
     }),
     name: "Grib2 " + strength,
     serverType: 'geoserver',
-    crossOrigin: 'anonymous'
+    crossOrigin: 'anonymous',
+    // projection: 'EPSG:4326'
   });
   return layer;
 }
@@ -67,26 +75,29 @@ for (var i = 0; i < checkboxesGrib2.length; i++) {
 }
 
 map.on('singleclick', function (evt) {
-
+  console.log(evt.coordinate);
   $('#tb').empty();
 
   var latLon = ol.proj.toLonLat(evt.coordinate, 'EPSG:3857');
-  var newLon = latLon[0] + 360;
-  // console.log(newLon);
-  // console.log(latLon);
+  console.log(latLon);
+  var newLon = latLon[0] - .75
+  var newLat = latLon[1] + .75;
+  // var newLon = 275.57;
+  // var newLat = 29.5;
+  console.log(newLon, newLat);
   var postData = `<?xml version="1.0" encoding="UTF-8"?><wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
     <ows:Identifier>gs:HvxValuesAtPointWpsReport</ows:Identifier>
     <wps:DataInputs>
       <wps:Input>
         <ows:Identifier>point</ows:Identifier>
         <wps:Data>
-          <wps:ComplexData mimeType="application/wkt"><![CDATA[POINT (` + newLon + ' ' + latLon[1] + `)]]></wps:ComplexData>
+          <wps:ComplexData mimeType="application/wkt"><![CDATA[POINT (` + newLon + ' ' + newLat + `)]]></wps:ComplexData>
         </wps:Data>
       </wps:Input>
       <wps:Input>
         <ows:Identifier>coverageNames</ows:Identifier>
         <wps:Data>
-          <wps:LiteralData>ncdc:VAR0-2-227_FROM_7-10--1_height_above_ground_120_Hour_Accumulation_probability_between_10p0_and_3,ncdc:VAR0-2-228_FROM_7-10--1_height_above_ground_120_Hour_Accumulation_probability_between_10p0_and_3,ncdc:VAR0-2-229_FROM_7-10--1_height_above_ground_120_Hour_Accumulation_probability_between_10p0_and_3,ncdc:VAR0-2-230_FROM_7-10--1_height_above_ground_120_Hour_Accumulation_probability_between_10p0_and_3</wps:LiteralData>
+          <wps:LiteralData>cite:test.25</wps:LiteralData>
         </wps:Data>
       </wps:Input>
       <wps:Input>
@@ -111,9 +122,10 @@ map.on('singleclick', function (evt) {
     dataType: 'json',
     data: postData,
   }).done(function (data) {
+    console.log(data);
     var features = data.features;
-    console.log(features);
-    console.log(features[0].properties);
+    // console.log(features);
+    // console.log(features[0].properties);
     var tableBody = document.getElementById('tb');
     var tr = tableBody.insertRow(-1);
     Object.keys(features[0].properties).forEach(function (key) {
