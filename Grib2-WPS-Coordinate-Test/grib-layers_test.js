1aa7e43ce3e9f63c60b132e602d858ac51b52d4b
Kyle Weingart <kyle.weingart@gmail.com>
2020-02-19 15:20:34 -0500

    working WPS and WMS requests

--- Grib2-WPS-Coordinate-Test/grib-layers_test.js
+++ Grib2-WPS-Coordinate-Test/grib-layers_test.js
@@ -18,7 +18,7 @@ var map = new ol.Map({
 var createGrib2Layer = (strength) => {
   
   console.log('Grib2layer');
-  var url = 'http://dev-hvx.hurrevac.com/geoserver/wms';
+  var url = 'https://dev-hvx.hurrevac.com/geoserver/wms';
   
   if (strength === 'ERTOA') {
     // console.log('strength works')
@@ -53,10 +53,10 @@ var createGrib2Layer = (strength) => {
 }
 
 var gribTestSource = new ol.source.TileWMS({
-  url: 'http://localhost:8080/geoserver/wms',
+  url: 'https://dev-hvx.hurrevac.com/geoserver/wms',
   params: {
-    // 'LAYERS': 'ncdc:' + layers,
-    'LAYERS': 'ncdc:AL632019_TOA_TOD_34kt_adv017_227',
+    
+    'LAYERS': 'nhc:AL052019_TOA_TOD_34kt_adv007_227',
     'TILED': true,
     'VERSION': '1.1.1',
     'FORMAT': 'image/png8',
@@ -64,18 +64,7 @@ var gribTestSource = new ol.source.TileWMS({
   serverType: 'geoserver',
   crossOrigin: 'anonymous',
 });
-var gribTestSource1 = new ol.source.TileWMS({
-  url: 'http://localhost:8080/geoserver/wms',
-  params: {
-    'LAYERS': 'ncdc:AL632019_TOA_TOD_34kt_adv017_228_test',
-    // 'LAYERS': 'ncdc:AL052019_TOA_TOD_34kt_adv007_228_test',
-    'TILED': true,
-    'VERSION': '1.1.1',
-    'FORMAT': 'image/png8',
-  },
-  serverType: 'geoserver',
-  crossOrigin: 'anonymous',
-});
+
 
 
 var checkboxesGrib2 = document.forms["windprobsGrib2Form"].elements["windprobsGrib2"];
@@ -100,17 +89,19 @@ map.on('singleclick', function (evt) {
   console.log(evt.coordinate);
   $('#tb').empty();
   
-  // this is the request for the original data - 0 to 360(its still necessary to edit coordinates to get proper data returned)
+  // this is the request for the -180 to 180 data in dev geoserver
 
   var latLon = ol.proj.toLonLat(evt.coordinate, 'EPSG:3857');
   console.log(latLon);
-  // var testLon = -86.50;
-  // var testLat = 32.5;
-  var newLon = latLon[0] + 359.25;
-  // i think this should be add .5
-  var newLat = latLon[1] + .75;
-  // var newLon = 275.57;
-  // var newLat = 29.5;
+ 
+  // with coordinate correction for WPS
+  // var newLon = latLon[0] - .75;
+  // var newLat = latLon[1] + .75;
+
+  // without coordinate correction for WPS
+  var newLon = latLon[0]; 
+  var newLat = latLon[1];
+ 
   console.log(newLon, newLat);
   var postData = `<?xml version="1.0" encoding="UTF-8"?><wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
   <ows:Identifier>gs:HvxValuesAtPointWpsReport</ows:Identifier>
@@ -124,7 +115,7 @@ map.on('singleclick', function (evt) {
       <wps:Input>
         <ows:Identifier>coverageNames</ows:Identifier>
         <wps:Data>
-          <wps:LiteralData>ncdc:AL632019_TOA_TOD_34kt_adv017_228</wps:LiteralData>
+          <wps:LiteralData>nhc:AL052019_TOA_TOD_34kt_adv007_227,nhc:AL052019_TOA_TOD_34kt_adv007_228,nhc:AL052019_TOA_TOD_34kt_adv007_229,nhc:AL052019_TOA_TOD_34kt_adv007_230</wps:LiteralData>
         </wps:Data>
       </wps:Input>
       <wps:Input>
@@ -141,7 +132,7 @@ map.on('singleclick', function (evt) {
     </wps:ResponseForm>
   </wps:Execute>`;
 
-  var url = 'http://localhost:8080/geoserver/wps';
+  var url = 'https://dev-hvx.hurrevac.com/geoserver/wps';
   $.ajax({
     type: 'POST',
     url: url,
@@ -149,10 +140,7 @@ map.on('singleclick', function (evt) {
     dataType: 'json',
     data: postData,
   }).done(function (data) {
-    console.log(data);
     var features = data.features;
-    // console.log(features);
-    // console.log(features[0].properties);
     var tableBody = document.getElementById('tb');
     var tr = tableBody.insertRow(-1);
     Object.keys(features[0].properties).forEach(function (key) {
@@ -174,126 +162,29 @@ map.on('singleclick', function (evt) {
     }
   })
 
-  // this is a wps request on the newest data - this grib2 has been corrected so that its data is exactly the same as the source data when the conversion to -180 to 180 is performed. 
-
-  var newLon2 = latLon[0] - .75;
-  // i think this should be add .5
-  var newLat2 = latLon[1] + .75;
-
-  var postData2 = `<?xml version="1.0" encoding="UTF-8"?><wps:Execute version="1.0.0" service="WPS" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.opengis.net/wps/1.0.0" xmlns:wfs="http://www.opengis.net/wfs" xmlns:wps="http://www.opengis.net/wps/1.0.0" xmlns:ows="http://www.opengis.net/ows/1.1" xmlns:gml="http://www.opengis.net/gml" xmlns:ogc="http://www.opengis.net/ogc" xmlns:wcs="http://www.opengis.net/wcs/1.1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xsi:schemaLocation="http://www.opengis.net/wps/1.0.0 http://schemas.opengis.net/wps/1.0.0/wpsAll.xsd">
-    <ows:Identifier>gs:HvxValuesAtPointWpsReport</ows:Identifier>
-    <wps:DataInputs>
-      <wps:Input>
-        <ows:Identifier>point</ows:Identifier>
-        <wps:Data>
-          <wps:ComplexData mimeType="application/wkt"><![CDATA[POINT (` + newLon2 + ' ' + newLat2 + `)]]></wps:ComplexData>
-        </wps:Data>
-      </wps:Input>
-      <wps:Input>
-        <ows:Identifier>coverageNames</ows:Identifier>
-        <wps:Data>
-          <wps:LiteralData>ncdc:AL632019_TOA_TOD_34kt_adv017_228_test</wps:LiteralData>
-        </wps:Data>
-      </wps:Input>
-      <wps:Input>
-        <ows:Identifier>dimensionName</ows:Identifier>
-        <wps:Data>
-          <wps:LiteralData>time</wps:LiteralData>
-        </wps:Data>
-      </wps:Input>
-    </wps:DataInputs>
-    <wps:ResponseForm>
-      <wps:RawDataOutput mimeType="application/json">
-        <ows:Identifier>result</ows:Identifier>
-      </wps:RawDataOutput>
-    </wps:ResponseForm>
-  </wps:Execute>`;
-
-  var url = 'http://localhost:8080/geoserver/wps';
-  $.ajax({
-    type: 'POST',
-    url: url,
-    contentType: 'text/xml',
-    dataType: 'json',
-    data: postData2,
-  }).done(function (data) {
-    console.log(data);
-    var features = data.features;
-    // console.log(features);
-    // console.log(features[0].properties);
-    var tableBody = document.getElementById('tb');
-    var tr = tableBody.insertRow(-1);
-    Object.keys(features[0].properties).forEach(function (key) {
-      if (key !== 'band') {
-        var epochTime = features[0].properties[key] * 1000;
-        console.log('WPStest: ' + epochTime);
-        var d = new Date(epochTime);
-        console.log(d);
-        tr.insertCell(-1).innerText = d;
-      }
-    });
-
-  }).fail(function () {
-    var colLength = document.getElementById('table-report').rows[0].cells.length;
-    var tableBody = document.getElementById('tb');
-    var tr = tableBody.insertRow(-1);
-    for (var i = 0; i < colLength; i++) {
-      tr.insertCell(-1).innerText = 'X';
-    }
-  })
-
-  console.log(map.getView().getResolution());
-
   var parser = new ol.format.WMSGetFeatureInfo();
 
-  wmsTimeArray = [];
-  
-    // var urlWMS = gribTestSource.getGetFeatureInfoUrl(evt.coordinate, map.getView().getResolution(), map.getView().getProjection(),
-    //   {
-    //     'INFO_FORMAT': 'application/vnd.ogc.gml',
-    //   });
-    // $.ajax({
-    //   type: 'GET',
-    //   url: urlWMS
-    // }).done(function (data) {
+    var urlWMS = gribTestSource.getGetFeatureInfoUrl(evt.coordinate, map.getView().getResolution(), map.getView().getProjection(),
+      {
+        'INFO_FORMAT': 'application/vnd.ogc.gml',
+      });
+    $.ajax({
+      type: 'GET',
+      url: urlWMS
+    }).done(function (data) {
      
-    //   var features = parser.readFeatures(data);
-    //   console.log(features[0].H);
-    //   Object.keys(features[0].H).forEach(function (key) {
-    //     if (key !== 'band') {
-    //       var epochTime = features[0].H[key] * 1000;
-    //       console.log('227: ' + epochTime);
-    //       var d = new Date(epochTime);
-    //       console.log(d);
-    //       // tr.insertCell(-1).innerText = d;
-    //     }
-    //   });
+      var features = parser.readFeatures(data);
+      console.log(features[0].H);
+      Object.keys(features[0].H).forEach(function (key) {
+        if (key !== 'band') {
+          var epochTime = features[0].H[key] * 1000;
+          console.log('227: ' + epochTime);
+          var d = new Date(epochTime);
+          console.log(d);
+        }
+      });
   
-    // })
-
-    var urlWMS = gribTestSource1.getGetFeatureInfoUrl(evt.coordinate, map.getView().getResolution(), map.getView().getProjection(),
-    {
-      'INFO_FORMAT': 'application/vnd.ogc.gml',
-    });
-  $.ajax({
-    type: 'GET',
-    url: urlWMS
-  }).done(function (data) {
-    
-    var features = parser.readFeatures(data);
-    
-    console.log(features[0].H);
-    Object.keys(features[0].H).forEach(function (key) {
-      if (key !== 'band') {
-        var epochTime = features[0].H[key] * 1000;
-        console.log('228: ' + epochTime);
-        var d = new Date(epochTime);
-        console.log(d);
-        // tr.insertCell(-1).innerText = d;
-      }
-    });
-
-  })
+    })
 });
 
 
