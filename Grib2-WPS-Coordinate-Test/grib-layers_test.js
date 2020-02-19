2b169f92ca2990f3c82424a5086cbf084333e82f
Kyle Weingart <kyle.weingart@gmail.com>
2020-02-19 14:37:12 -0500

    added data wms layer

--- Grib2-WPS-Coordinate-Test/grib-layers_test.js
+++ Grib2-WPS-Coordinate-Test/grib-layers_test.js
@@ -29,7 +29,7 @@ var createGrib2Layer = (strength) => {
   } else if (strength === 'MLTOD') {
     var layers = 'AL052019_TOA_TOD_34kt_adv007_229'
   } else if (strength === 'LRTOD') {
-    var layers = 'VAR0-2-230_FROM_7-10--1_height_above_ground_120_Hour_Accumulation_probability_between_10p0_and_3'
+    var layers = 'AL052019_TOA_TOD_34kt_adv007_230'
   } else if (strength === 'test') {
     var layers = 'ncdc:AL632019_TOA_TOD_34kt_adv017_228'
     // var layers = 'ncdc:AL632019_TOA_TOD_34kt_adv017_228'
