775799ce15e97496767a4315e2f8ea50d086120f
Kyle Weingart <kyle.weingart@gmail.com>
2020-07-21 11:25:28 -0400

    added popup with prob

--- QC_Probabilistic_Reports/state-county-label.js
+++ QC_Probabilistic_Reports/state-county-label.js
@@ -1,3 +1,21 @@
+// elements that make up the popup
+
+// var container = $('#popup');
+var container = document.getElementById('popup');
+// var content = $('#popup-content');
+var content = document.getElementById('popup-content');
+// var closer = $('#popup-closer');
+// var closer = document.getElementById('popup-closer');
+
+// create an overlay to anchor the popup to the  map
+
+var overlay = new ol.Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250
+  }
+});
 
 // basemap grayscale
 var basemap = new ol.layer.Tile({
@@ -33,10 +51,44 @@ const testLayer = new ol.layer.Image({
   });
 
 var map = new ol.Map({
-    layers: [basemap, wmsCurrentTigerImage, testLayer],
+    layers: [wmsCurrentTigerImage, testLayer],
     target: document.getElementById('map'),
     view: new ol.View({
         center: ol.proj.transform([-87.5, 31], 'EPSG:4326', 'EPSG:3857'),
         zoom: 3
     }),
+    overlays: [overlay]
 });
+
+var parser = new ol.format.WMSGetFeatureInfo();
+var viewResolution = map.getView().getResolution();
+var viewProjection = map.getView().getProjection();
+
+
+map.on('singleclick', function (evt) {
+    console.log(evt.coordinate);
+    var url = testLayer.getSource().getGetFeatureInfoUrl(evt.coordinate, viewResolution, viewProjection,
+      {
+        'INFO_FORMAT': 'application/vnd.ogc.gml',
+      });
+    $.ajax({
+      type: 'GET',
+      url: url
+    }).done(function (data) {
+        console.log(data);
+      var features = parser.readFeatures(data);
+      console.log(features);
+      if (features.length > 0) {
+        console.log(features);
+        var prob = features[0].H.GRAY_INDEX;
+        console.log(prob);
+        overlay.setPosition(evt.coordinate);
+        content.innerText = features[0].H.GRAY_INDEX;
+        container.style.display = 'block';
+      } else {
+        featureOverlay.getSource().clear();
+        container.style.display = 'none';
+      }
+  
+    })
+  });
