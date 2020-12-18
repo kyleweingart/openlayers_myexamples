0878f20a08da050d7a2b812ee9159d107e6e99d8
Kyle Weingart <kyle.weingart@gmail.com>
2020-12-18 14:34:49 -0500

    added logic to raster source client side rendering

--- QPF_Rainfall/dist/QPF_Rainfall.e31bb0bc.js
+++ QPF_Rainfall/dist/QPF_Rainfall.e31bb0bc.js
@@ -93611,6 +93611,18 @@ var _OSM = _interopRequireDefault(require("ol/source/OSM"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 // import {Fill, Stroke, Style} from 'ol/style';
 var timeObject = {
   hourSix: require('./hourSix.kml'),
@@ -93742,16 +93754,22 @@ function getTotalDepth(inputs, data) {
 
   for (var i = 0; i < inputs.length; i++) {
     dataArray.push(inputs[i].data);
-  }
+  } // i need to work on this to get the total 
 
+
+  console.log(dataArray);
   var max = dataArray.reduce(function (final, current) {
+    console.log('????????????????????????????');
+
     for (var i = 0; i < final.length; i = i + 4) {
-      //Only compare if current isn't completely transparent
+      console.log(final.length); //Only compare if current isn't completely transparent
+
       if (current[i + 3] > 0) {
         var current_rgba = [current[i], current[i + 1], current[i + 2], current[i + 3]];
         var final_rgba = [final[i], final[i + 1], final[i + 2], final[i + 3]];
-        var current_depth = toDepth(current_rgba, colorMap);
-        var final_depth = toDepth(final_rgba, colorMap);
+        var current_depth = toDepth(current_rgba, colorMap); // console.log(current_depth);
+
+        var final_depth = toDepth(final_rgba, colorMap); // console.log(final_depth);
 
         if (typeof current_depth === "undefined") {
           current_depth = 0;
@@ -93759,19 +93777,46 @@ function getTotalDepth(inputs, data) {
 
         if (typeof final_depth === "undefined") {
           final_depth = 0;
+        } // console.log(current_depth + final_depth);
+        // console.log(`Rainfall = ${current_depth + final_depth}`);
+
+
+        var rainfall = "".concat(current_depth + final_depth); // console.log(rainfall);
+
+        for (var _i = 0, _Object$entries = Object.entries(colorMap); _i < _Object$entries.length; _i++) {
+          var _Object$entries$_i = _slicedToArray(_Object$entries[_i], 2),
+              rgba = _Object$entries$_i[0],
+              depth = _Object$entries$_i[1];
+
+          // console.log(depth);
+          // console.log(rainfall);
+          if (depth == rainfall) {
+            // console.log('==================');
+            // this is where to assign the new rgba values that correspond to the value in the color map
+            // need to add some fail safes, etc. 
+            var rgbaArray = rgba.split(','); // console.log(rgbaArray);
+            // console.log(rgbaArray[0]);
+            // console.log(rgbaArray[1]);
+            // console.log(rgbaArray[2]);
+          }
         }
 
         if (current_depth > final_depth) {
           final[i] = current[i]; //R
+          // console.log(final[i]);
 
           final[i + 1] = current[i + 1]; //G
+          // console.log(final[i+1]);
 
           final[i + 2] = current[i + 2]; //B
+          // console.log(final[i+2]);
 
           final[i + 3] = current[i + 3]; //A
+          // console.log(`A:${final[i+3]}`);
         }
       }
-    }
+    } // console.log(final);
+
 
     return final;
   });
@@ -93783,8 +93828,6 @@ function getTotalDepth(inputs, data) {
 }
 
 function toDepth(rgb, colorMap) {
-  console.log('here');
-
   function within(val, target, range) {
     return val > target - range && val < target + range;
   }
@@ -93852,27 +93895,11 @@ var createMosaicLayer = function createMosaicLayer(time) {
     imageOps: false
   });
   var surgeImage = new _layer.Image({
-    source: raster
+    source: raster,
+    name: "rainfallMosaic" + time
   });
-  return surgeImage; // var layer = new TileLayer({
-  //   source: new TileWMS({
-  //     url: url,
-  //     params: {
-  //       'LAYERS': 'cite:rainfall_mosaic',
-  //       'TILED': true,
-  //       'VERSION': '1.1.1',
-  //       'FORMAT': 'image/png8',
-  //       'projection': 'EPSG:3857',
-  //       'TIME': days[i]
-  //     }
-  //   }),
-  //   name: "rainfallMosaic" + time,
-  //   serverType: 'geoserver',
-  //   crossOrigin: 'anonymous'
-  // });
-  // return layer;
-}; // }
-
+  return surgeImage;
+};
 
 var checkboxesRainfallMosaic = document.forms["rainfallMosaicForm"].elements["rainfallMosaic"];
 
@@ -93886,6 +93913,65 @@ for (var i = 0; i < checkboxesRainfallGrib.length; i++) {
     } else {
       map.getLayers().forEach(function (layer) {
         if (layer.get('name') === 'rainfallMosaic' + timePeriod) {
+          map.removeLayer(layer);
+        }
+      });
+    }
+  };
+} //  example trying to use time dimension as interval range rather than list. 
+
+
+var createMosaicIntervalLayer = function createMosaicIntervalLayer(time) {
+  // not quite getting this to work 
+  // i am having two issues that need to be solved: 
+  // 1. Can i request multiple granules with one request using a date range?  So far i've tried doing this using / ,etc with no sucess
+  // 2. If I request multiple ranges can i get the return values to be added together?
+  console.log('interval');
+  var url = 'http://localhost:8080/geoserver/wms';
+
+  if (time === '24') {
+    // var period = '2020-12-14T00:00:00Z'
+    var period = 'P3D/2020-12-15T00:00:00Z';
+  } else if (time === '48') {
+    // var period = '2020-12-13T00:00:00Z/2020-12-15T00:00:00Z'
+    var period = 'P2D/2020-12-15T00:00:00Z';
+  } else if (time === '72') {
+    // var period = '2020-12-12/2020-12-14'
+    var period = 'P3D/PRESENT';
+  } // const urlnew = 'http://localhost:8080/geoserver/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng8&TRANSPARENT=true&LAYERS=cite%3Arainfall_mosaic&TILED=true&projection=EPSG%3A3857&TIME=2020-12-12T00:00:00Z/2020-12-14T00:00:00Z';
+
+
+  var layer = new _layer.Tile({
+    source: new _TileWMS.default({
+      url: url,
+      params: {
+        'LAYERS': 'cite:rainfall_mosaic',
+        'TILED': true,
+        'VERSION': '1.1.1',
+        'FORMAT': 'image/png8',
+        'projection': 'EPSG:3857',
+        'TIME': period
+      }
+    }),
+    name: "rainfallIntervalMosaic" + time,
+    serverType: 'geoserver',
+    crossOrigin: 'anonymous'
+  });
+  return layer;
+};
+
+var checkboxesRainfallIntervalMosaic = document.forms["rainfallMosaicIntervalForm"].elements["rainfallMosaicInterval"];
+
+for (var i = 0; i < checkboxesRainfallGrib.length; i++) {
+  checkboxesRainfallIntervalMosaic[i].onclick = function () {
+    var timePeriod = this.value;
+
+    if (this.checked === true) {
+      var mapLayer = createMosaicIntervalLayer(timePeriod);
+      map.addLayer(mapLayer);
+    } else {
+      map.getLayers().forEach(function (layer) {
+        if (layer.get('name') === 'rainfallIntervalMosaic' + timePeriod) {
           map.removeLayer(layer);
         }
       });
@@ -93931,7 +94017,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49839" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63781" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- QPF_Rainfall/index.js
+++ QPF_Rainfall/index.js
@@ -129,15 +129,22 @@ function getTotalDepth(inputs, data) {
     dataArray.push(inputs[i].data);
   }
 
+  // i need to work on this to get the total 
+  console.log(dataArray);
+
   var max = dataArray.reduce(function(final, current) {
+    console.log('????????????????????????????');
     for (var i = 0; i < final.length; i = i+4) {
+      console.log(final.length);
       //Only compare if current isn't completely transparent
       if (current[i+3] > 0) {
         var current_rgba = [current[i], current[i+1], current[i+2], current[i+3]];
         var final_rgba = [final[i], final[i+1], final[i+2], final[i+3]];
 
         var current_depth = toDepth(current_rgba, colorMap);
+        // console.log(current_depth);
         var final_depth = toDepth(final_rgba, colorMap);
+        // console.log(final_depth);
 
         if (typeof current_depth === "undefined") {
           current_depth = 0;
@@ -145,22 +152,47 @@ function getTotalDepth(inputs, data) {
         if (typeof final_depth === "undefined") {
           final_depth = 0;
         }
+        // console.log(current_depth + final_depth);
+        // console.log(`Rainfall = ${current_depth + final_depth}`);
+        const rainfall = `${current_depth + final_depth}`
+
+        // console.log(rainfall);
+
+        for (const [rgba, depth] of Object.entries(colorMap)) {
+          // console.log(depth);
+          // console.log(rainfall);
+          if (depth == rainfall) {
+            // console.log('==================');
+            // this is where to assign the new rgba values that correspond to the value in the color map
+            // need to add some fail safes, etc. 
+            const rgbaArray = rgba.split(',');
+            // console.log(rgbaArray);
+            // console.log(rgbaArray[0]);
+            // console.log(rgbaArray[1]);
+            // console.log(rgbaArray[2]);
+          }
+        }
 
         if (current_depth > final_depth) {
           final[i] = current[i];     //R
+          // console.log(final[i]);
           final[i+1] = current[i+1]; //G
+          // console.log(final[i+1]);
           final[i+2] = current[i+2]; //B
+          // console.log(final[i+2]);
           final[i+3] = current[i+3]; //A
+          // console.log(`A:${final[i+3]}`);
         }
       }
     }
+    // console.log(final);
     return final;
   });
   return {data: max, width: width, height: height};
 }
 
 function toDepth(rgb, colorMap) {
-  console.log('here');
+  
   function within(val, target, range) {
     return (val > target - range && val < target + range)
   }
@@ -227,47 +259,87 @@ var createMosaicLayer = (time) => {
   });
 
   var surgeImage = new ImageLayer({
-    source: raster
+    source: raster,
+    name: "rainfallMosaic" + time,
   });
 
   return surgeImage;
-    // var layer = new TileLayer({
-    //   source: new TileWMS({
-    //     url: url,
-    //     params: {
-    //       'LAYERS': 'cite:rainfall_mosaic',
-    //       'TILED': true,
-    //       'VERSION': '1.1.1',
-    //       'FORMAT': 'image/png8',
-    //       'projection': 'EPSG:3857',
-    //       'TIME': days[i]
-    //     }
-    //   }),
-    //   name: "rainfallMosaic" + time,
-    //   serverType: 'geoserver',
-    //   crossOrigin: 'anonymous'
-    // });
-    // return layer;
-   
   }
 
-    // }
-
-
   
-
+  var checkboxesRainfallMosaic = document.forms["rainfallMosaicForm"].elements["rainfallMosaic"];
+  for (var i = 0; i < checkboxesRainfallGrib.length; i++) {
+    checkboxesRainfallMosaic[i].onclick = function () {
+      var timePeriod = this.value;
+      if (this.checked === true) {
+        var mapLayer = createMosaicLayer(timePeriod);
+        map.addLayer(mapLayer);
+      } else {
+        map.getLayers().forEach(function (layer) {
+          if (layer.get('name') === 'rainfallMosaic' + timePeriod) {
+            map.removeLayer(layer);
+          }
+        })
+        
+      }
+    }
+  }
   
+  //  example trying to use time dimension as interval range rather than list. 
 
-var checkboxesRainfallMosaic = document.forms["rainfallMosaicForm"].elements["rainfallMosaic"];
+var createMosaicIntervalLayer = (time) => {
+
+  // not quite getting this to work 
+  // i am having two issues that need to be solved: 
+
+      // 1. Can i request multiple granules with one request using a date range?  So far i've tried doing this using / ,etc with no sucess
+      // 2. If I request multiple ranges can i get the return values to be added together?
+
+  console.log('interval');
+  var url = 'http://localhost:8080/geoserver/wms';
+
+  if (time === '24') {
+      // var period = '2020-12-14T00:00:00Z'
+      var period = 'P3D/2020-12-15T00:00:00Z'
+  }   else if (time === '48') {
+      // var period = '2020-12-13T00:00:00Z/2020-12-15T00:00:00Z'
+      var period = 'P2D/2020-12-15T00:00:00Z'
+    }   else if (time === '72') {
+      // var period = '2020-12-12/2020-12-14'
+      var period = 'P3D/PRESENT'
+  }
+
+  // const urlnew = 'http://localhost:8080/geoserver/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image%2Fpng8&TRANSPARENT=true&LAYERS=cite%3Arainfall_mosaic&TILED=true&projection=EPSG%3A3857&TIME=2020-12-12T00:00:00Z/2020-12-14T00:00:00Z';
+
+  var layer = new TileLayer({
+      source: new TileWMS({
+          url: url,
+          params: {
+              'LAYERS': 'cite:rainfall_mosaic',
+              'TILED': true,
+              'VERSION': '1.1.1',
+              'FORMAT': 'image/png8',
+              'projection': 'EPSG:3857',
+              'TIME': period
+          }
+      }),
+      name: "rainfallIntervalMosaic" + time,
+      serverType: 'geoserver',
+      crossOrigin: 'anonymous'
+  });
+  return layer;
+}
+
+var checkboxesRainfallIntervalMosaic = document.forms["rainfallMosaicIntervalForm"].elements["rainfallMosaicInterval"];
 for (var i = 0; i < checkboxesRainfallGrib.length; i++) {
-  checkboxesRainfallMosaic[i].onclick = function () {
+  checkboxesRainfallIntervalMosaic[i].onclick = function () {
     var timePeriod = this.value;
     if (this.checked === true) {
-      var mapLayer = createMosaicLayer(timePeriod);
+      var mapLayer = createMosaicIntervalLayer(timePeriod);
       map.addLayer(mapLayer);
     } else {
       map.getLayers().forEach(function (layer) {
-        if (layer.get('name') === 'rainfallMosaic' + timePeriod) {
+        if (layer.get('name') === 'rainfallIntervalMosaic' + timePeriod) {
           map.removeLayer(layer);
         }
       })
